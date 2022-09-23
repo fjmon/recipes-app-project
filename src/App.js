020b53d16bd219a53a3d49990a0557a8202436ee
Gabriela Moura <gabrieladaniel49@hotmail.com>
2022-09-23 17:17:28 -0300

    :bug: resolvendo conflito

--- src/App.js
+++ src/App.js
@@ -5,24 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="meals">
-      <span className="logo">TRYBE 2.0</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
-=======
     <Provider>
       <main>
         <h1>teste</h1>
       </main>
     </Provider>
->>>>>>> 03705fca84b9e37895b3914b1d5eb4c3d5f76e4a
   );
 }
 
