198a6e7ce927b8c5dc16e2e4e1bd9a5541a19d5a
Joao Guilherme <jguilherme.marcos@gmail.com>
2022-09-23 22:30:02 -0300

    implementa footer (req 16 a 18)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import Provider from './context/MyContext';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Provider from './context/MyProvider';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Provider>
-      <main>
-        <h1>teste</h1>
-      </main>
+      <BrowserRouter>
+        <Route exact path="/" />
+        <Route exact path="/meals" component={ Footer } />
+        <Route exact path="/drinks" component={ Footer } />
+        <Route exact path="/meals/{id-da-receita}" />
+        <Route exact path="/drinks/{id-da-receita}" />
+        <Route exact path="/meals/{id-da-receita}/in-progress" />
+        <Route exact path="/drinks/{id-da-receita}/in-progress" />
+        <Route exact path="/profile" component={ Footer } />
+        <Route exact path="/done-recipes" />
+        <Route exact path="/favorite-recipes" />
+        <main>
+          <h1>teste</h1>
+        </main>
+      </BrowserRouter>
     </Provider>
   );
 }
