ac7cd230131a80fae66f07ea7d02b224a01b7a6d
luizfilipelgs <luizfilipelgs@gmail.com>
2022-09-26 20:26:23 -0300

    testes header 100%

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,6 +1,6 @@
 function SearchBar() {
   return (
-    <div id="search-bar">
+    <div id="search-bar" data-testid="search-bar">
       <div id="container-radios">
         <label htmlFor="ingrediente">
           <input
