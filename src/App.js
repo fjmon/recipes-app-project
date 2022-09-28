4dbd1446f0ce9c11b01f5324068b1d7cc1aee789
Hian <hian.hsm@gmail.com>
2022-09-28 18:31:38 -0300

    Requisitos 24 e 25

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 import Footer from './components/Footer';
 import Recipes from './pages/Recipes';
-import RecipesDetails from './pages/RecipesDetails';
-import DrinksDetails from './pages/DrinksDetails';
+import RecipeDetails from './pages/RecipeDetails';
+import DrinkDetails from './pages/DrinkDetails';
 
 function App() {
   return (
@@ -30,7 +30,7 @@ function App() {
                 <Footer />
               </div>) }
           />
-          <Route exact path="/meals/:id" component={ RecipesDetails } />
+          <Route exact path="/meals/:id" component={ RecipeDetails } />
           <Route
             exact
             path="/drinks"
@@ -41,7 +41,7 @@ function App() {
                 <Footer />
               </div>) }
           />
-          <Route exact path="/drinks/:id" component={ DrinksDetails } />
+          <Route exact path="/drinks/:id" component={ DrinkDetails } />
           <Route
             exact
             path="/profile"
@@ -53,8 +53,6 @@ function App() {
           />
           <Route exact path="/done-recipes" component={ DoneRecipes } />
           <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
-          <Route exact path="/meals/:id" component={ RecipeDetails } />
-          <Route exact path="/drinks/:id" component={ RecipeDetails } />
           <Route exact path="/meals/:id/in-progress" />
           <Route exact path="/drinks/:id/in-progress" />
         </Switch>
--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,44 +1,68 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 function RecipeDetails() {
   const { id } = useParams();
-  const { pathname } = useLocation();
-  const [details, setDetails] = useState(null);
+  const [recipeDetails, setRecipeDetails] = useState({});
 
-  const fetchMeal = async () => {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-    const data = response.json();
-    setDetails(data.meals);
+  const embedURL = (url) => {
+    if (url) {
+      const URL = url;
+      const newURL = URL.replace('watch?v=', 'embed/');
+      return newURL;
+    }
   };
-
-  const fetchDrink = async () => {
-    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-    const data = response.json();
-    setDetails(data.drinks);
-  };
-
-  const param = new URLSearchParams(pathname).has('/meals');
 
   useEffect(() => {
-    if (param) {
-      fetchMeal();
-    } else {
-      fetchDrink();
+    const fetchMeal = async () => {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const data = await response.json();
+      setRecipeDetails(data.meals[0]);
+    };
+    fetchMeal();
+  }, [id]);
+
+  const getIngredients = () => {
+    const ingredients = [];
+    const maxIngredients = 20;
+    for (let index = 0; index <= maxIngredients; index += 1) {
+      const ingredient = `strIngredient${index}`;
+      const measure = `strMeasure${index}`;
+      if (recipeDetails[ingredient] && recipeDetails[measure] !== null) {
+        ingredients.push(`${recipeDetails[ingredient]} (${recipeDetails[measure]}) `);
+      }
     }
-  });
+    return ingredients;
+  };
 
   return (
     <>
-    { param ? (
-      <>
-      <img data-testid="recipe-photo" src={details.strMealThumb} />
-      <h1 data-testid="recipe-title">{details.strMeal}</h1>
-      <h3 data-testid="recipe-category">{details.strCategory}</h3>
-      
-      </>
-    ) : 
-    }
+      <img
+        data-testid="recipe-photo"
+        src={ recipeDetails.strMealThumb }
+        alt={ recipeDetails.strMeal }
+      />
+      <h1 data-testid="recipe-title">{recipeDetails.strMeal}</h1>
+      <h3 data-testid="recipe-category">{recipeDetails.strCategory}</h3>
+      <ol>
+        <h6>Ingredients:</h6>
+        { getIngredients().map((item, index) => (
+          <li
+            key={ index }
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            {item}
+          </li>
+        ))}
+      </ol>
+      <p data-testid="instructions">{recipeDetails.strInstructions}</p>
+      { embedURL(recipeDetails.strYoutube)
+        && <iframe
+          src={ embedURL(recipeDetails.strYoutube) }
+          title={ recipeDetails.strMeal }
+          allowFullScreen
+          data-testid="video"
+        />}
     </>
   );
 }
