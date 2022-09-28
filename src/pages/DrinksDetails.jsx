bc66a0f6e40514d7a328b653952387f32cb4219d
Gabriela Moura <gabrieladaniel49@hotmail.com>
2022-09-28 20:25:11 -0300

    :sparkles: iniciando requisitos 26 e 27

--- src/pages/DrinksDetails.jsx
+++ src/pages/DrinksDetails.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 export default function DrinksDetails() {
+  const fetchApiMeals = async () => {
+    const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log('API endpoint not found');
+    }
+  };
+
+  // const api = fetchApiMeals();
+  // {api.meals}
   return (
     <div>DrinksDetails</div>
   );
--- src/pages/RecipesDetails.jsx
+++ src/pages/RecipesDetails.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 export default function RecipesDetails() {
+  const fetchApiDrinks = async () => {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log('API endpoint not found');
+    }
+  };
+
+  // const api = fetchApiDrinks();
+  // {api.drinks}
   return (
     <div>RecipesDetails</div>
   );
