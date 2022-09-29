0d2f72790a3dc6af2f434723f7b5c64c55d807aa
Gabriela Moura <gabrieladaniel49@hotmail.com>
2022-09-28 21:29:01 -0300

    :bug: removendo conflitos

--- src/pages/DrinksDetails.jsx
+++ src/pages/DrinksDetails.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function DrinksDetails() {
+function DrinkDetails() {
+  const { id } = useParams();
+  const [drinkDetails, setDrinkDetails] = useState({});
+
   const fetchApiMeals = async () => {
     const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
     try {
@@ -14,10 +18,52 @@ export default function DrinksDetails() {
 
   const api = fetchApiMeals();
 
+  useEffect(() => {
+    const fetchDrink = async () => {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const data = await response.json();
+      setDrinkDetails(data.drinks[0]);
+    };
+    fetchDrink();
+  }, [id]);
+
+  const getIngredients = () => {
+    const ingredients = [];
+    const maxIngredients = 15;
+    for (let index = 0; index <= maxIngredients; index += 1) {
+      const ingredient = `strIngredient${index}`;
+      const measure = `strMeasure${index}`;
+      if (drinkDetails[ingredient] && drinkDetails[measure] !== null) {
+        ingredients.push(`${drinkDetails[ingredient]} (${drinkDetails[measure]}) `);
+      }
+    }
+    return ingredients;
+  };
+
   return (
-    <div>
-      DrinksDetails
+    <>
+      <img
+        data-testid="recipe-photo"
+        src={ drinkDetails.strDrinkThumb }
+        alt={ drinkDetails.strDrink }
+      />
+      <h1 data-testid="recipe-title">{drinkDetails.strDrink}</h1>
+      <h3 data-testid="recipe-category">{drinkDetails.strAlcoholic}</h3>
+      <ol>
+        <h6>Ingredients:</h6>
+        { getIngredients().map((item, index) => (
+          <li
+            key={ index }
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            {item}
+          </li>
+        ))}
+      </ol>
+      <p data-testid="instructions">{drinkDetails.strInstructions}</p>
       {api.meals}
-    </div>
+    </>
   );
 }
+
+export default DrinkDetails;
--- src/pages/RecipesDetails.jsx
+++ src/pages/RecipesDetails.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function RecipesDetails() {
+function RecipeDetails() {
+  const { id } = useParams();
+  const [recipeDetails, setRecipeDetails] = useState({});
+
   const fetchApiDrinks = async () => {
     const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
     try {
@@ -13,10 +17,68 @@ export default function RecipesDetails() {
   };
 
   const api = fetchApiDrinks();
+
+  const embedURL = (url) => {
+    if (url) {
+      const URL = url;
+      const newURL = URL.replace('watch?v=', 'embed/');
+      return newURL;
+    }
+  };
+
+  useEffect(() => {
+    const fetchMeal = async () => {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const data = await response.json();
+      setRecipeDetails(data.meals[0]);
+    };
+    fetchMeal();
+  }, [id]);
+
+  const getIngredients = () => {
+    const ingredients = [];
+    const maxIngredients = 20;
+    for (let index = 0; index <= maxIngredients; index += 1) {
+      const ingredient = `strIngredient${index}`;
+      const measure = `strMeasure${index}`;
+      if (recipeDetails[ingredient] && recipeDetails[measure] !== null) {
+        ingredients.push(`${recipeDetails[ingredient]} (${recipeDetails[measure]}) `);
+      }
+    }
+    return ingredients;
+  };
+
   return (
-    <div>
-      RecipesDetails
+    <>
+      <img
+        data-testid="recipe-photo"
+        src={ recipeDetails.strMealThumb }
+        alt={ recipeDetails.strMeal }
+      />
+      <h1 data-testid="recipe-title">{recipeDetails.strMeal}</h1>
+      <h3 data-testid="recipe-category">{recipeDetails.strCategory}</h3>
+      <ol>
+        <h6>Ingredients:</h6>
+        { getIngredients().map((item, index) => (
+          <li
+            key={ index }
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            {item}
+          </li>
+        ))}
+      </ol>
+      <p data-testid="instructions">{recipeDetails.strInstructions}</p>
+      { embedURL(recipeDetails.strYoutube)
+        && <iframe
+          src={ embedURL(recipeDetails.strYoutube) }
+          title={ recipeDetails.strMeal }
+          allowFullScreen
+          data-testid="video"
+        />}
       {api.drinks}
-    </div>
+    </>
   );
 }
+
+export default RecipeDetails;
