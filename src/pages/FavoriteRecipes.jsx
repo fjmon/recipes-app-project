4415829afeb8793a5a19bd12fa796522ab90b430
Hian <hian.hsm@gmail.com>
2022-09-30 18:02:06 -0300

    Requisito 50

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import iconFavorited from '../images/blackHeartIcon.svg';
 
 function FavoriteRecipes() {
   const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     const getFavorites = () => {
-      const favoriteRecipes = localStorage.getItem('favoriteRecipes');
+      const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
       setFavorites(favoriteRecipes);
     };
     getFavorites();
@@ -24,7 +25,40 @@ function FavoriteRecipes() {
 
       {favorites.length > 0 && favorites.map((fav, index) => (
         <div key={ index }>
-          <img src={ fav.image } alt={ fav.name } />
+          <img
+            src={ fav.image }
+            alt={ fav.name }
+            data-testid={ `${index}-horizontal-image` }
+          />
+
+          <h6
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            {fav.strMeal ? fav.strMeal : fav.strDrink }
+          </h6>
+
+          <p
+            data-testid={ `${index}-horizontal-name` }
+          >
+            {fav.strCategory}
+          </p>
+
+          <div>
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-share-btn` }
+            >
+              Share
+            </button>
+
+            <img
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              src={ iconFavorited }
+              alt="Favoritado"
+              role="presentation"
+            />
+          </div>
+
         </div>
       ))}
 
